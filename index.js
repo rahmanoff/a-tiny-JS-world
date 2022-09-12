a47cd264888644388ecb1078ff2cf3718bbf786d
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-09-12 18:16:08 +0300

    v1

--- index.js
+++ index.js
@@ -8,16 +8,44 @@ import { print } from './js/lib.js';
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-const dog = {
-   species: 'dog',
-   name: 'Toby',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'woof-woof'
-};
-
-
+const inhabitants = [
+{
+      species: 'dog',
+      name: 'Toby',
+      gender: 'male',
+      legs: 4,
+      hands: 0,
+      saying: 'woof-woof',
+      friends: ['Mary', 'John'],
+      },
+{
+      species: 'cat',
+      name: 'Alice',
+      gender: 'male',
+      legs: 4,
+      hands: 0,
+      saying: 'meow',
+      friends: ['Mary', 'John'],
+   },
+{
+      species: 'woman',
+      name: 'Mary',
+      gender: 'female',
+      legs: 2,
+      hands: 2,
+      saying: 'Hi, John!',
+      friends: ['John', 'Alice', 'Toby'],
+   },
+{
+      species: 'man',
+      name: 'John',
+      gender: 'male',
+      legs: 2,
+      hands: 2,
+      saying: 'Hi, Mary!',
+      friends: ['Mary', 'Toby', 'Alice'],
+   },
+];
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -37,4 +65,22 @@ const dog = {
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-  print(dog.species + '; ' + dog.name + '; ' + dog.gender + '; ' + dog.legs + '; ' + dog.hands + '; ' + dog.saying);
+
+//   inhabitants.dog.species + '; ' 
+//    + inhabitants.dog.name + '; ' 
+//    + inhabitants.dog.gender + '; ' 
+//    + inhabitants.dog.legs + '; ' 
+//    + inhabitants.dog.hands + '; ' 
+//    + inhabitants.dog.saying
+
+for (let i = 0; i < inhabitants.length; i++) {
+   const species = inhabitants[i].species;
+   const name = inhabitants[i].name;
+   const gender = inhabitants[i].gender;
+   const legs = inhabitants[i].legs;
+   const hands = inhabitants[i].hands;
+   const saying = inhabitants[i].saying;
+   const friends = inhabitants[i].friends;
+
+   print('Inhabitant:' + species + '; Name:' + name + '; Gender:' + gender + '; Legs:' + legs + '; Hands:' + hands + '; Saying:' + saying + '; Friends:' + friends);
+}
